28b5954193012477e87ca6646bbeb2c8754afefd
Mohammad <mohammadrasel201@gmail.com>
2023-10-13 04:09:39 +0600

    complted 44.2

---
+++ src/assets/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import Link from "../Link/Link";
+
+
+const NavBar = () => {
+
+    const routes = [
+        { path: '/', name: 'Home', id: 1 },
+        { path: '/about', name: 'About', id: 2 },
+        { path: '/services', name: 'Services', id: 3 },
+        { path: '/contact', name: 'Contact', id: 4 },
+        { path: '*', name: 'NotFound', id: 5 },
+      ];
+      
+      
+    return (
+        <nav>
+            <ul className="md:flex">
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
